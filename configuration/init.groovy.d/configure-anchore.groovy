45c074f6fca3f495dbf874058fc5b789e77e4a70
Michael LaCourse <mlacours@redhat.com>
2020-03-06 13:29:50 -0500

    change anchore creds

--- configuration/init.groovy.d/configure-anchore.groovy
+++ configuration/init.groovy.d/configure-anchore.groovy
@@ -1,14 +1,11 @@
 #!/usr/bin/env groovy
 import jenkins.model.*
 import groovy.json.JsonSlurper
-import hudson.plugins.sonar.SonarInstallation
-import hudson.plugins.sonar.SonarRunnerInstallation
-import hudson.plugins.sonar.SonarRunnerInstaller
 import hudson.plugins.sonar.model.TriggersConfig
 import hudson.tools.InstallSourceProperty
 import com.cloudbees.plugins.credentials.*;
 import com.cloudbees.plugins.credentials.domains.Domain;
-import org.jenkinsci.plugins.plaincredentials.impl.StringCredentialsImpl;
+import com.cloudbees.plugins.credentials.impl.*
 
 
 import java.util.logging.Level
@@ -18,16 +15,17 @@ final def LOG = Logger.getLogger("LABS")
 
 LOG.log(Level.INFO, 'Configuring Anchore')
 
+anchoreUsername = System.getenv("ANCHORE_USERNAME") ?: "admin"
+anchorePassword = System.getenv("ANCHORE_PASSWORD") ?: "anchore"
 
-def token = hudson.util.Secret.fromString("anchore")
+usernameAndPassword = new UsernamePasswordCredentialsImpl(
+  CredentialsScope.GLOBAL,
+  "anchore-creds", "Anchore creds for Jenkins",
+  anchoreUsername,
+  anchorePassword
+)
 
-def secretText = new StringCredentialsImpl(
-                          CredentialsScope.GLOBAL,
-                          "anchore-creds",
-                          "Anchore API Access",
-                           token)
-
-SystemCredentialsProvider.instance.store.addCredentials(Domain.global(), secretText)
+SystemCredentialsProvider.instance.store.addCredentials(Domain.global(), usernameAndPassword)
 
     // Add the SonarQube server config to Jenkins
 
